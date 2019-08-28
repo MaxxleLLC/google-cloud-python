422e26f65bedad381a3df0b9f7adb37f836b04f4
IlyaFaer <ilya.faer@mail.ru>
2019-08-28 11:57:52 +0300

    Raw-code

--- bigquery/google/cloud/bigquery/_helpers.py
+++ bigquery/google/cloud/bigquery/_helpers.py
@@ -32,10 +32,23 @@ _TIMEONLY_WO_MICROS = "%H:%M:%S"
 _TIMEONLY_W_MICROS = "%H:%M:%S.%f"
 _PROJECT_PREFIX_PATTERN = re.compile(
     r"""
-    (?P<project_id>\S+\:[^.]+)\.(?P<dataset_id>[^.]+)\.(?P<custom_id>[^.]+)$
+    (?P<project_id>\S+\:[^.]+)\.(?P<dataset_id>[^.]+)(?:$|\.(?P<custom_id>[^.]+)$)
 """,
     re.VERBOSE,
 )
+
+
+def _parse(full_id):
+    with_prefix = _PROJECT_PREFIX_PATTERN.match(full_id)
+    if with_prefix is None:
+        parts = full_id.split(".")
+    else:
+        project_id = with_prefix.group("project_id")
+        dataset_id = with_prefix.group("dataset_id")
+        custom_id = with_prefix.group("custom_id")
+        parts = [project_id, dataset_id, custom_id]
+        parts = [part for part in parts if part]
+    return parts
 
 
 def _not_null(value, field):
@@ -597,14 +610,7 @@ def _parse_3_part_id(full_id, default_project=None, property_name="table_id"):
     output_project_id = default_project
     output_dataset_id = None
     output_resource_id = None
-    with_prefix = _PROJECT_PREFIX_PATTERN.match(full_id)
-    if with_prefix is None:
-        parts = full_id.split(".")
-    else:
-        project_id = with_prefix.group("project_id")
-        dataset_id = with_prefix.group("dataset_id")
-        custom_id = with_prefix.group("custom_id")
-        parts = [project_id, dataset_id, custom_id]
+    parts = _parse(full_id)
 
     if len(parts) != 2 and len(parts) != 3:
         raise ValueError(
