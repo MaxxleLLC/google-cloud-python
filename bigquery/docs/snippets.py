a1a9a8f936a5370b266e68b2b5d9f079ee20bfbc
Leonid Emar-Kar <l.emarkar@outlook.com>
2019-08-30 13:35:42 +0300

    load_table_from_uri_cmek

--- bigquery/docs/snippets.py
+++ bigquery/docs/snippets.py
@@ -541,49 +541,6 @@ def test_load_table_from_uri_json(client, to_delete, capsys):
     assert "Loaded 50 rows." in out
 
 
-def test_load_table_from_uri_cmek(client, to_delete):
-    dataset_id = "load_table_from_uri_cmek_{}".format(_millis())
-    dataset = bigquery.Dataset(client.dataset(dataset_id))
-    dataset.location = "US"
-    client.create_dataset(dataset)
-    to_delete.append(dataset)
-
-    # [START bigquery_load_table_gcs_json_cmek]
-    # from google.cloud import bigquery
-    # client = bigquery.Client()
-    # dataset_id = 'my_dataset'
-
-    dataset_ref = client.dataset(dataset_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.autodetect = True
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-
-    # Set the encryption key to use for the destination.
-    # TODO: Replace this key with a key you have created in KMS.
-    kms_key_name = "projects/{}/locations/{}/keyRings/{}/cryptoKeys/{}".format(
-        "cloud-samples-tests", "us-central1", "test", "test"
-    )
-    encryption_config = bigquery.EncryptionConfiguration(kms_key_name=kms_key_name)
-    job_config.destination_encryption_configuration = encryption_config
-    uri = "gs://cloud-samples-data/bigquery/us-states/us-states.json"
-
-    load_job = client.load_table_from_uri(
-        uri,
-        dataset_ref.table("us_states"),
-        location="US",  # Location must match that of the destination dataset.
-        job_config=job_config,
-    )  # API request
-
-    assert load_job.job_type == "load"
-
-    load_job.result()  # Waits for table load to complete.
-
-    assert load_job.state == "DONE"
-    table = client.get_table(dataset_ref.table("us_states"))
-    assert table.encryption_configuration.kms_key_name == kms_key_name
-    # [END bigquery_load_table_gcs_json_cmek]
-
-
 def test_load_table_from_uri_parquet(client, to_delete, capsys):
     dataset_id = "load_table_from_uri_parquet_{}".format(_millis())
     dataset = bigquery.Dataset(client.dataset(dataset_id))
