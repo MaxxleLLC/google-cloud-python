c141d2e786300c2fc6cfb02e6017b0163256c774
HemangChothani <hemang.chothani@qlogic.io>
2020-01-21 19:20:33 +0530

    refactor(bigquery): add extract table and table cmek operations

--- bigquery/docs/usage/encryption.rst
+++ bigquery/docs/usage/encryption.rst
@@ -10,7 +10,7 @@ in the BigQuery documentation for more details.
 Create a new table, using a customer-managed encryption key from
 Cloud KMS to encrypt it.
 
-.. literalinclude:: ../snippets.py
+.. literalinclude:: ../samples/create_table_cmek.py
    :language: python
    :dedent: 4
    :start-after: [START bigquery_create_table_cmek]
@@ -18,7 +18,7 @@ Cloud KMS to encrypt it.
 
 Change the key used to encrypt a table.
 
-.. literalinclude:: ../snippets.py
+.. literalinclude:: ../samples/update_table_cmek.py
    :language: python
    :dedent: 4
    :start-after: [START bigquery_update_table_cmek]
---
+++ bigquery/samples/extract_table_json.py
@@ -0,0 +1,59 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def extract_table_json(table_id):
+
+    # [START bigquery_extract_table_json]
+    import time
+
+    from google.cloud import bigquery
+    from google.cloud import storage
+
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    # Construct a Storage client object.
+    storage_client = storage.Client()
+
+    # TODO(developer): Set table_id to the ID of the model to fetch.
+    # table_id = 'your-project.your_dataset.your_table'
+
+    bucket_name = "extract_shakespeare_json_{}".format(int(time.time() * 1000))
+    bucket = storage_client.create_bucket(bucket_name)
+
+    destination_uri = "gs://{}/{}".format(bucket_name, "shakespeare.json")
+
+    job_config = bigquery.job.ExtractJobConfig(
+        destination_format=bigquery.DestinationFormat.NEWLINE_DELIMITED_JSON
+    )
+    extract_job = client.extract_table(
+        table_id,
+        destination_uri,
+        job_config=job_config,
+        # Must match the source table location.
+        location="US",
+    )  # Make an API request.
+    extract_job.result()  # Waits for job to complete.
+
+    table = client.get_table(table_id)
+    print(
+        "Exported {}.{}.{} to {}".format(
+            table.project, table.dataset_id, table.table_id, destination_uri
+        )
+    )
+    # [END bigquery_extract_table_json]
+
+    blob = bucket.get_blob("shakespeare.json")
+    return blob, bucket
